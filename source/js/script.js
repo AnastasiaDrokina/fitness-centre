62d20727866256d5afefe7ee20820b4cb3893f0a
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2020-02-12 14:17:15 +0100

    Add js btn subscription

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,21 @@
 'use strict';
 var btnBanner = document.querySelector('.btn__banner');
 var subscription = document.querySelector('.subscription');
+var subscriptionBtns = document.querySelectorAll('.subscription__btn');
 
+console.log(subscriptionBtns);
+
+// Scroll
 btnBanner.addEventListener('click', function () {
   window.scrollBy({top: subscription.offsetTop - window.scrollY, behavior: 'smooth'});
+});
+
+// Add class "active" on subscription__btn
+subscriptionBtns.forEach(function (subscriptionBtn) {
+  subscriptionBtn.addEventListener('click', function () {
+    subscriptionBtns.forEach(function (btn) {
+      btn.classList.remove('subscription__btn--active');
+    });
+    subscriptionBtn.classList.add('subscription__btn--active');
+  });
 });
