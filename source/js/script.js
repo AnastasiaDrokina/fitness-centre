e096fabd339f19ab3d4f9ce899e58f2c0d0600e5
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2020-02-17 22:43:33 +0100

    Fix errors

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,7 @@
 var btnBanner = document.querySelector('.btn--banner');
 var subscription = document.querySelector('#subscription');
 var subscriptionBtns = document.querySelectorAll('.subscription__btn');
+var subscriptionsLists = document.querySelectorAll('.subscription__list');
 
 // Scroll
 btnBanner.addEventListener('click', function (evt) {
@@ -21,8 +22,9 @@ subscriptionBtns.forEach(function (subscriptionBtn) {
 });
 
 // Add slider trainer
-var sliderTrainer = new Swiper ('.slider--trainer', {
+var sliderTrainer = new Swiper('.slider--trainer', {
   slidesPerView: 1,
+  slidesPerGroup: 1,
   navigation: {
     nextEl: '.trainer-button-next',
     prevEl: '.trainer-button-prev'
@@ -30,20 +32,32 @@ var sliderTrainer = new Swiper ('.slider--trainer', {
   breakpoints: {
     768: {
       slidesPerView: 2,
+      slidesPerGroup: 2,
       spaceBetween: 20
     },
     1200: {
       slidesPerView: 4,
+      slidesPerGroup: 4,
       spaceBetween: 40
     }
   }
 });
 
 // Add slider review
-var sliderReview = new Swiper ('.slider--review', {
+var sliderReview = new Swiper('.slider--review', {
   navigation: {
     nextEl: '.review-button-next',
     prevEl: '.review-button-prev'
   },
 });
 
+// Add tabs subscription
+subscriptionBtns.forEach(function (btn, index) {
+  btn.addEventListener('click', function () {
+    subscriptionsLists.forEach(function (list) {
+      list.classList.remove('subscription__list--active');
+    });
+
+    subscriptionsLists[index].classList.add('subscription__list--active');
+  });
+});
